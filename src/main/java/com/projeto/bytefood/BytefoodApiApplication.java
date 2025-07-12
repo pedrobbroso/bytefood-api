d22be4f49cd798efff1195387872f12e96000bf2
Pedro Balbastro Broso <pedro.broso@gmail.com>
2025-07-12 16:59:17 -0300

    criando projeto

---
+++ src/main/java/com/projeto/bytefood/BytefoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.bytefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BytefoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BytefoodApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/bytefood/BytefoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.bytefood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BytefoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
